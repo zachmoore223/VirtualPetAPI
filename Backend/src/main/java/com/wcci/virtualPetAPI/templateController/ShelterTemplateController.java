e865cd01ca22ef0d10fb4287da5a703180bcf113
Zach Moore <zachmoore223@gmail.com>
2023-03-06 10:58:20 -0500

    templateController

---
+++ Backend/src/main/java/com/wcci/virtualPetAPI/templateController/ShelterTemplateController.java
@@ -0,0 +1,31 @@
+package com.wcci.virtualPetAPI.templateController;
+
+import com.wcci.virtualPetAPI.repositories.ShelterRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class ShelterTemplateController {
+    final private ShelterRepository shelterRepository;
+
+    public ShelterTemplateController(@Autowired ShelterRepository shelterRepository) {
+        this.shelterRepository = shelterRepository;
+    }
+
+    @GetMapping("/shelters")
+    public String getShelters(Model model) {
+        model.addAttribute("shelters", shelterRepository.findAll());
+        return "shelters.html";
+    }
+
+    @GetMapping("/shelters/{shelter_id}")
+    public String getShelter(Model model, @PathVariable long shelter_id) {
+        model.addAttribute("shelter", shelterRepository.findById(shelter_id).get());
+        model.addAttribute("pets", shelterRepository.findById(shelter_id).get().getAllPets());
+        return "shelter.html";
+    }
+
+}
