7f7006d8b8c413b59ac853e28384d3e08559f641
Zach Moore <zachmoore223@gmail.com>
2023-03-11 12:02:44 -0500

    added restController annotation

--- Backend/src/main/java/com/wcci/virtualPetAPI/restControllers/RoboticDogController.java
+++ Backend/src/main/java/com/wcci/virtualPetAPI/restControllers/RoboticDogController.java
@@ -7,6 +7,7 @@ import com.wcci.virtualPetAPI.repositories.ShelterRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@RestController
 public class RoboticDogController {
     final ShelterRepository shelterRepository;
     final NamedPetRepository namedPetRepository;
